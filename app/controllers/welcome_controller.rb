3269cc02f714a272038d0e92e6711ff3ba46f698
Étienne Michon <etienne@scalingo.com>
2017-12-24 15:48:29 +0100

    Add more pictures

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @fortune = pick_random_line
+    @image = [
+      'moi_debile.jpg', 'moi.jpg', 'Belette qui tete.jpg', 'titi_dodo.jpg',
+      'oim.JPG', 'photox 28.jpg', 'Soirée tarte flambée.jpg',
+    ].sample
   end
 
   private
